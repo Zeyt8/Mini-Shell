32957f8d6804fff288587ff67db77a30fbb6ea1a
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2023-05-07 16:49:58 +0300

    Update cmd.c

---
+++ content/assignments/minishell/src/cmd.c
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "cmd.h"
+#include "utils.h"
+
+#define READ		0
+#define WRITE		1
+
+/**
+ * Internal change-directory command.
+ */
+static bool shell_cd(word_t *dir)
+{
+	/* TODO: Execute cd. */
+
+	return 0;
+}
+
+/**
+ * Internal exit/quit command.
+ */
+static int shell_exit(void)
+{
+	/* Execute exit/quit. */
+	int ret = exit(1);
+
+	return ret;
+}
+
+/**
+ * Parse a simple command (internal, environment variable assignment,
+ * external command).
+ */
+static int parse_simple(simple_command_t *s, int level, command_t *father)
+{
+	/* Sanity checks. */
+	if (s == NULL)
+		return SHELL_EXIT;
+	/* If builtin command, execute the command. */
+	if (strcmp(s->verb->string, "cd") == 0) {
+		return shell_cd(s->params);
+	}
+	if (strcmp(s->verb->string, "exit") == 0 || strcmp(s->verb->string, "quit") == 0) {
+		return shell_exit();
+	}
+	/* TODO: If variable assignment, execute the assignment and return
+	 * the exit status.
+	 */
+
+	/* If external command:
+	 *   1. Fork new process
+	 *     2c. Perform redirections in child
+	 *     3c. Load executable in child
+	 *   2. Wait for child
+	 *   3. Return exit status
+	 */
+	int pid = fork();
+	if (pid == 0) {
+		// child
+		// redirect
+		if (s->in != NULL) {
+			int fd = open(s->in->string, O_RDONLY);
+			dup2(fd, STDIN_FILENO);
+			close(fd);
+		}
+		if (s->out != NULL) {
+			int fd = open(s->out->string, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
+		// execute
+		execvp(s->verb->string, (char *const *)s->params);
+		exit(1);
+	} else {
+		// parent
+		int status;
+		waitpid(pid, &status, 0);
+		return WEXITSTATUS(status);
+	}
+}
+
+/**
+ * Process two commands in parallel, by creating two children.
+ */
+static bool run_in_parallel(command_t *cmd1, command_t *cmd2, int level,
+		command_t *father)
+{
+	/* TODO: Execute cmd1 and cmd2 simultaneously. */
+
+	return true; /* TODO: Replace with actual exit status. */
+}
+
+/**
+ * Run commands by creating an anonymous pipe (cmd1 | cmd2).
+ */
+static bool run_on_pipe(command_t *cmd1, command_t *cmd2, int level,
+		command_t *father)
+{
+	/* TODO: Redirect the output of cmd1 to the input of cmd2. */
+
+	return true; /* TODO: Replace with actual exit status. */
+}
+
+/**
+ * Parse and execute a command.
+ */
+int parse_command(command_t *c, int level, command_t *father)
+{
+	/* TODO: sanity checks */
+	if (c == NULL)
+		return SHELL_EXIT;
+
+	if (c->op == OP_NONE) {
+		/* TODO: Execute a simple command. */
+
+		return 0; /* TODO: Replace with actual exit code of command. */
+	}
+
+	switch (c->op) {
+	case OP_SEQUENTIAL:
+		/* TODO: Execute the commands one after the other. */
+		break;
+
+	case OP_PARALLEL:
+		/* TODO: Execute the commands simultaneously. */
+		break;
+
+	case OP_CONDITIONAL_NZERO:
+		/* TODO: Execute the second command only if the first one
+		 * returns non zero.
+		 */
+		break;
+
+	case OP_CONDITIONAL_ZERO:
+		/* TODO: Execute the second command only if the first one
+		 * returns zero.
+		 */
+		break;
+
+	case OP_PIPE:
+		/* TODO: Redirect the output of the first command to the
+		 * input of the second.
+		 */
+		break;
+
+	default:
+		return SHELL_EXIT;
+	}
+
+	return 0; /* TODO: Replace with actual exit code of command. */
+}
